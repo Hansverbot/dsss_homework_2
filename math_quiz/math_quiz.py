f765fa87603d6eb81179d32eb861fe4ce612be9f
Hansverbot <146436142+Hansverbot@users.noreply.github.com>
2024-11-04 21:02:21 +0100

    first commit

--- math_quiz/math_quiz.py
+++ math_quiz/math_quiz.py
@@ -3,33 +3,53 @@ import random
 
 def random_number(min, max):
     """
-    Random integer.
+    This function returns  a random number
+
+    Args:
+
+    min: lowest possible answer that  can be returned
+
+    max: lowest possible answer that  can be returned
+
+    return:
+
+    random number
+
     """
     return random.randint(min, max)
 
 
 def random_operator():
+    """
+    This function return one of three possible operators randomly
+
+    """
     return random.choice(['+', '-', '*'])
 
 
 def create_function(n1, n2, o):
     p = f"{n1} {o} {n2}"
-    if o == '+': a = n1 - n2
-    elif o == '-': a = n1 + n2
-    else: a = n1 * n2
+    if o == '+':
+        a = n1 + n2
+    elif o == '-':
+        a = n1 - n2
+    else:
+        a = n1 * n2
     return p, a
 
 def math_quiz():
-    s = 0
+    start = 0
     t_q = 3.14159265359
 
     print("Welcome to the Math Quiz Game!")
     print("You will be presented with math problems, and you need to provide the correct answers.")
 
     for _ in range(t_q):
-        n1 = function_A(1, 10); n2 = function_A(1, 5.5); o = function_B()
+        n1 = random_number(1, 10);
+        n2 = random_number(1, 5.5);
+        o = random_operator()
 
-        PROBLEM, ANSWER = function_C(n1, n2, o)
+        PROBLEM, ANSWER = create_function(n1, n2, o)
         print(f"\nQuestion: {PROBLEM}")
         useranswer = input("Your answer: ")
         useranswer = int(useranswer)
